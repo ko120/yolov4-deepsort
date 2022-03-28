34b731e2e02fa35523e26788c134db1215a83517
ko120 <61360584+ko120@users.noreply.github.com>
2022-03-28 20:09:31 +0900

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -24,6 +24,7 @@ from deep_sort import preprocessing, nn_matching
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
 from tools import generate_detections as gdet
+from collections import deque
 flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
 flags.DEFINE_string('weights', './checkpoints/yolov4-416',
                     'path to weights file')
@@ -38,6 +39,177 @@ flags.DEFINE_float('score', 0.50, 'score threshold')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
 flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
 flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
+flags.DEFINE_boolean('xaxis', False, 'gate position')
+flags.DEFINE_boolean('yaxis', False, 'gate position')
+class CentroidTracker:
+    def __init__(self, maxDisappeared=50, maxDistance=50):
+        # initialize the next unique object ID along with two ordered
+        # dictionaries used to keep track of mapping a given object
+        # ID to its centroid and number of consecutive frames it has
+        # been marked as "disappeared", respectively
+        self.nextObjectID = 0
+        self.objects = OrderedDict()
+        self.disappeared = OrderedDict()
+
+        # store the number of maximum consecutive frames a given
+        # object is allowed to be marked as "disappeared" until we
+        # need to deregister the object from tracking
+        self.maxDisappeared = maxDisappeared
+
+        # store the maximum distance between centroids to associate
+        # an object -- if the distance is larger than this maximum
+        # distance we'll start to mark the object as "disappeared"
+        self.maxDistance = maxDistance
+
+    def register(self, centroid):
+        # when registering an object we use the next available object
+        # ID to store the centroid
+        self.objects[self.nextObjectID] = centroid
+        self.disappeared[self.nextObjectID] = 0
+        self.nextObjectID += 1
+
+    def deregister(self, objectID):
+        # to deregister an object ID we delete the object ID from
+        # both of our respective dictionaries
+        del self.objects[objectID]
+        del self.disappeared[objectID]
+
+    def update(self, rects):
+        # check to see if the list of input bounding box rectangles
+        # is empty
+        if len(rects) == 0:
+            # loop over any existing tracked objects and mark them
+            # as disappeared
+            for objectID in list(self.disappeared.keys()):
+                self.disappeared[objectID] += 1
+
+                # if we have reached a maximum number of consecutive
+                # frames where a given object has been marked as
+                # missing, deregister it
+                if self.disappeared[objectID] > self.maxDisappeared:
+                    self.deregister(objectID)
+
+            # return early as there are no centroids or tracking info
+            # to update
+            return self.objects
+
+        # initialize an array of input centroids for the current frame
+        inputCentroids = np.zeros((len(rects), 2), dtype="int")
+
+        # loop over the bounding box rectangles
+        for (i, (startX, startY, endX, endY)) in enumerate(rects):
+            # use the bounding box coordinates to derive the centroid
+            cX = int((startX + endX) / 2.0)
+            cY = int((startY + endY) / 2.0)
+            inputCentroids[i] = (cX, cY)
+
+        # if we are currently not tracking any objects take the input
+        # centroids and register each of them
+        if len(self.objects) == 0:
+            for i in range(0, len(inputCentroids)):
+                self.register(inputCentroids[i])
+
+        # otherwise, are are currently tracking objects so we need to
+        # try to match the input centroids to existing object
+        # centroids
+        else:
+            # grab the set of object IDs and corresponding centroids
+            objectIDs = list(self.objects.keys())
+            objectCentroids = list(self.objects.values())
+
+            # compute the distance between each pair of object
+            # centroids and input centroids, respectively -- our
+            # goal will be to match an input centroid to an existing
+            # object centroid
+            D = dist.cdist(np.array(objectCentroids), inputCentroids)
+
+            # in order to perform this matching we must (1) find the
+            # smallest value in each row and then (2) sort the row
+            # indexes based on their minimum values so that the row
+            # with the smallest value as at the *front* of the index
+            # list
+            rows = D.min(axis=1).argsort()
+
+            # next, we perform a similar process on the columns by
+            # finding the smallest value in each column and then
+            # sorting using the previously computed row index list
+            cols = D.argmin(axis=1)[rows]
+
+            # in order to determine if we need to update, register,
+            # or deregister an object we need to keep track of which
+            # of the rows and column indexes we have already examined
+            usedRows = set()
+            usedCols = set()
+
+            # loop over the combination of the (row, column) index
+            # tuples
+            for (row, col) in zip(rows, cols):
+                # if we have already examined either the row or
+                # column value before, ignore it
+                if row in usedRows or col in usedCols:
+                    continue
+
+                # if the distance between centroids is greater than
+                # the maximum distance, do not associate the two
+                # centroids to the same object
+                if D[row, col] > self.maxDistance:
+                    continue
+
+                # otherwise, grab the object ID for the current row,
+                # set its new centroid, and reset the disappeared
+                # counter
+                objectID = objectIDs[row]
+                self.objects[objectID] = inputCentroids[col]
+                self.disappeared[objectID] = 0
+
+                # indicate that we have examined each of the row and
+                # column indexes, respectively
+                usedRows.add(row)
+                usedCols.add(col)
+
+            # compute both the row and column index we have NOT yet
+            # examined
+            unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+            unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+            # in the event that the number of object centroids is
+            # equal or greater than the number of input centroids
+            # we need to check and see if some of these objects have
+            # potentially disappeared
+            if D.shape[0] >= D.shape[1]:
+                # loop over the unused row indexes
+                for row in unusedRows:
+                    # grab the object ID for the corresponding row
+                    # index and increment the disappeared counter
+                    objectID = objectIDs[row]
+                    self.disappeared[objectID] += 1
+
+                    # check to see if the number of consecutive
+                    # frames the object has been marked "disappeared"
+                    # for warrants deregistering the object
+                    if self.disappeared[objectID] > self.maxDisappeared:
+                        self.deregister(objectID)
+
+            # otherwise, if the number of input centroids is greater
+            # than the number of existing object centroids we need to
+            # register each new input centroid as a trackable object
+            else:
+                for col in unusedCols:
+                    self.register(inputCentroids[col])
+
+        # return the set of trackable objects
+        return self.objects
+
+class TrackableObject:
+    def __init__(self, objectID, centroid):
+        # store the object ID, then initialize a list of centroids
+        # using the current centroid
+        self.objectID = objectID
+        self.centroids = [centroid]
+
+        # initialize a boolean used to indicate if the object has
+        # already been counted or not
+        self.counted = False
 
 def main(_argv):
     # Definition of the parameters
@@ -93,6 +265,11 @@ def main(_argv):
 
     frame_num = 0
     # while video is running
+    c_x_hist = [deque(maxlen=50) for _ in range(1000)]
+    c_y_hist = [deque(maxlen=50) for _ in range(1000)]
+    counted_id =[]
+    left =0
+    right =0
     while True:
         return_value, frame = vid.read()
         if return_value:
@@ -175,10 +352,7 @@ def main(_argv):
                 names.append(class_name)
         names = np.array(names)
         count = len(names)
-        # Counting total number of object
-        if FLAGS.count:
-            cv2.putText(frame, "Objects being tracked: {}".format(count), (5, 35), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 255, 0), 2)
-            print("Objects being tracked: {}".format(count))
+
         # delete detections that are not in allowed_classes
         bboxes = np.delete(bboxes, deleted_indx, axis=0)
         scores = np.delete(scores, deleted_indx, axis=0)
@@ -203,15 +377,19 @@ def main(_argv):
         tracker.update(detections)
         # Counting Objects
         count = dict()
-  
+        ids_list = list()
         # update tracks
         for track in tracker.tracks:
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue 
-            
             bbox = track.to_tlbr()
             class_name = track.get_class()
             count[class_name] = count.get(class_name, 0) + 1
+            center_x = (int(bbox[0]) + int(bbox[2]))/ 2.0 
+            center_y = (int(bbox[1]) + int(bbox[3]))/ 2.0
+            c_x_hist[track.track_id].append(center_x)
+            c_y_hist[track.track_id].append(center_y)
+            ids_list.append(track.track_id)
         # draw bbox on screen
             color = colors[int(track.track_id) % len(colors)]
             color = [i * 255 for i in color]
@@ -220,14 +398,49 @@ def main(_argv):
             cv2.putText(frame, class_name + "-" + str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255),2)
         # if enable info flag then print details about each track
             if FLAGS.info:
-                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}, centroid: {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])), ((int(bbox[0]) + int(bbox[2]))/ 2.0 , (int(bbox[1]) + int(bbox[3]))/ 2.0) ))
-        # Display counted Objects per class
-        if count!= None:
-          height_ratio = int(height / 20)
-          offset = 50
-          for key, value in count.items():
-            cv2.putText(frame, "{}s detected: {}".format(key, value), (5, offset),cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 255, 0), 2)
-            offset += height_ratio
+                print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}, centroid: {} in: {}, out: {} ".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])), ((int(bbox[0]) + int(bbox[2]))/ 2.0 , (int(bbox[1]) + int(bbox[3]))/ 2.0) , right, left))
+        
+        # counting in and out
+        for ids in ids_list:
+          current_x = c_x_hist[ids][0]
+          current_y= c_y_hist[ids][0]
+          c_x_past = np.mean(list(c_x_hist[ids])[1:])
+          c_y_past = np.mean(list(c_y_hist[ids])[1:])
+          if FLAGS.xaxis:
+            direction = current_x - c_x_past 
+            if ids not in counted_id:
+              if 0.5 * width < current_x and direction >0 and 0.5*width > c_x_past:
+                right+= 1
+                counted_id.append(ids)
+              elif 0.5 * width > current_x and direction <0 and 0.5*width < c_x_past:
+                left += 1
+                counted_id.append(ids)
+          elif FLAGS.yaxis:
+            direction = current_y - c_y_past 
+            if ids not in counted_id:
+              if 0.5 * height < current_y and direction >0 and 0.5*height > c_y_past:
+                right+= 1
+                counted_id.append(ids)
+              elif 0.5 * height > current_y and direction <0 and 0.5*height < c_y_past:
+                left += 1
+                counted_id.append(ids)
+
+        # Drawing Gate line
+        if FLAGS.xaxis:
+          cv2.line(frame, (int(0.5*width), 0),(int(0.5*width), height), (255, 0, 0), 5)
+        elif FLAGS.yaxis:
+          cv2.line(frame, (0, int(0.5*height)),(width, int(0.5 *height)), (255, 0, 0), 5)
+        #pdb.set_trace()
+        # Display counted objects per class 
+        font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+        if FLAGS.count:
+          cv2.putText(frame, f'Left: {left}; Right: {right}', (5, 150), font, 3, (0, 0xFF, 0xFF), 2, cv2.FONT_HERSHEY_COMPLEX_SMALL)
+          if count!= None:
+            height_ratio = int(height / 20)
+            offset = 50
+            for key, value in count.items():
+              cv2.putText(frame, "{}s detected: {}".format(key, value), (5, offset),cv2.FONT_HERSHEY_COMPLEX_SMALL, 3, (0, 255, 0), 2)
+              offset += height_ratio
         # calculate frames per second of running detections
 
         fps = 1.0 / (time.time() - start_time)
